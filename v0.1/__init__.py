b9c01e54f6946a86eaa71f9005c475227a6c7a12
blenderlounge <blenderlounge@gmail.com>
2015-10-11 15:46:15 +0200

    Update __init__.py

--- v0.1/__init__.py
+++ v0.1/__init__.py
@@ -1,20 +1,70 @@
 # -*- coding: utf-8 -*-
 
-import bpy
-import sys
-import os
-from os.path import join
+'''
+Copyright (C) 2015 YOUR NAME
+YOUR@MAIL.com
 
-if __name__ == '__main__':
-    material = sys.argv[5] # récupère le nom du matériau
-    thumbnails_directory = sys.argv[6] # récupère le dossier de stockage des miniatures
-    
-    bpy.data.objects["_Render_Model"].active_material = bpy.data.materials[material]
+Created by YOUR NAME
 
-    bpy.ops.render.render()
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+bl_info = {
+    "name": "Blender Shader Library (BSL)",
+    "description": "Create your own shader library, with thumbnail preview and a simple import",
+    "author": "Lapineige, Pitiwazou, Pistiwique",
+    "version": (0, 1, 0),
+    "blender": (2, 75, 0),
+    "location": "3D View / Material Properties",
+    "warning": "This addon is still in development - quite stable, but be carefull with your materials",
+    "wiki_url": "",
+    "category": "Material" }
     
-    print('[BSL] - Preview Created:', join(thumbnails_directory, material + '.jpeg'))
     
-    bpy.data.images['Render Result'].save_render(filepath=join(thumbnails_directory, material + '.jpeg'))
+import bpy      
+
+
+# load and reload submodules
+##################################    
+
+#from . import developer_utils  # ne marche pas toujours pour moi - Lapi
+#modules = developer_utils.setup_addon_modules(__path__, __name__
+
+
+# register
+################################## 
+
+import traceback
+from . draw import register_pcoll_preview, unregister_pcoll_preview, BSL_AddMaterial
     
-    bpy.ops.wm.quit_blender()
+def register():
+    try: bpy.utils.register_module(__name__)
+    except: traceback.print_exc()
+    
+    bpy.types.Cycles_PT_context_material.append(BSL_AddMaterial)
+    register_pcoll_preview()
+    
+    bpy.types.WindowManager.is_generating_preview = bpy.props.BoolProperty(default=False)
+    
+    #print("Registered {} with {} modules".format(bl_info["name"], len(modules)))
+
+def unregister():
+    unregister_pcoll_preview()
+    
+    try: bpy.utils.unregister_module(__name__)
+    except: traceback.print_exc()
+    
+    bpy.types.Cycles_PT_context_material.remove(BSL_AddMaterial)
+    
+    #print("Unregistered {}".format(bl_info["name"]))
